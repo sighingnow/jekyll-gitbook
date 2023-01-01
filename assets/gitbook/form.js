20ab8755279d21905efd568b390b61c1c1c82f2a
Dmitry Bokatov <d.bokatov@gmail.com>
2023-01-01 21:13:42 +0100

    Update form.js

--- assets/gitbook/form.js
+++ assets/gitbook/form.js
@@ -4,12 +4,12 @@ const saveButton = document.querySelector("#save"); // select save button
 const alertBox = document.querySelector(".alert"); // select alert display div
 let form = document.querySelector(`#${formId}`); // select form
 let formElements = form.elements; // get the elements in the form
-const element = "attendee-id";
+const formElement = "attendee-id";
 
 
 saveButton.onclick = event => {
   event.preventDefault();
-  let inputAttendee= document.getElementById(element);
+  let inputAttendee= document.getElementById(formElement);
   localStorage.setItem(formId, inputAttendee.value);
   const message = "Attendee ID has been saved!";
   displayAlert(message);
@@ -36,20 +36,13 @@ const displayAlert = message => {
  */
 const populateForm = () => {
   if (localStorage.key(formId)) {
-    const savedData = JSON.parse(localStorage.getItem(formId)); // get and parse the saved data from localStorage
-    for (const element of formElements) {
-      if (element.name in savedData) {
-        element.value = savedData[element.name];
+    const savedData = localStorage.getItem(formId); 
+    formElement.value = savedData[formElement.name];
       }
     }
     const message = "Form has been refilled with saved data!";
     displayAlert(message);
   }
 };
-
-function myFunction() {
-  const savedAttendy = JSON.parse(localStorage.getItem(formId));
-  document.getElementById("attendee").innerHTML = savedAttendy;
-}
 
 document.onload = populateForm(); // populate the form when the document is loaded
