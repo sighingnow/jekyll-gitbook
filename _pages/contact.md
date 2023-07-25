c6b449151ff44dbe53789561880bafca71d0230f
chalex-eth <chalard.alex@gmail.com>
2023-07-25 11:33:54 +0200

    Add mermaid files and build compile

--- _pages/contact.md
+++
@@ -1,8 +0,0 @@
----
-title: Contact
-author: Tao He
-date: 2022-02-05
-category: Jekyll
-layout: post
----
-
---
+++ build_doc.sh
@@ -0,0 +1,5 @@
+
+mmdc -i mermaid_files/StandardDeposit.mmd -o illustrations/StandardDeposit.svg
+mmdc -i mermaid_files/GovernanceDeposit.mmd -o illustrations/GovernanceDeposit.svg
+mmdc -i mermaid_files/StandardWithdraw.mmd -o illustrations/StandardWithdraw.svg
+mmdc -i mermaid_files/GovernanceWithdraw.mmd -o illustrations/GovernanceWithdraw.svg
